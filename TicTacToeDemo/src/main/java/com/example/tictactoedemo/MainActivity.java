24b348903d260af82ba9f702951dd02c0fa60d98
snixer724 <snixer724@yahoo.com>
2014-02-23 16:09:14 -0500

    Added music

--- TicTacToeDemo/src/main/java/com/example/tictactoedemo/MainActivity.java
+++ TicTacToeDemo/src/main/java/com/example/tictactoedemo/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.tictactoedemo;
 
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -21,16 +18,11 @@ public class MainActivity extends ActionBarActivity {
     TextView oText;
     Player currPlayer;
     Button[] buttons;
+    BackgroundSound music;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
 
         b = new Board();
 
@@ -111,6 +103,7 @@ public class MainActivity extends ActionBarActivity {
                 updateScore();
             }
         });
+        music = new BackgroundSound();
     }
 
     private void updateScore() {
@@ -125,6 +118,16 @@ public class MainActivity extends ActionBarActivity {
             b.setEnabled(true);
         }
         currPlayer = Player.X;
+    }
+
+    public void onResume() {
+        super.onResume();
+        music.execute(null);
+    }
+
+    public void onPause() {
+        super.onPause();
+        music.cancel(true);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -146,20 +149,17 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
+    public class BackgroundSound extends AsyncTask<Void, Void, Void> {
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
+        protected Void doInBackground(Void... params) {
+            MediaPlayer player = MediaPlayer.create(MainActivity.this, R.raw.forsakendemotictactoe);
+            player.setLooping(true); // Set looping
+            player.setVolume(100,100);
+            player.start();
 
+            return null;
+        }
+
+    }
 }
